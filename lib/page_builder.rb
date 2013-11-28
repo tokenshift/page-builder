89348c859374652c1f116816754a8479941d250d
Nathan Clark <Nathan.Clark@tokenshift.com>
2013-11-27 20:02:36 -0500

    Using Rake::Task class method to define task.

--- lib/page_builder.rb
+++ lib/page_builder.rb
@@ -1,6 +1,5 @@
 require 'page-builder/folder'
 require 'page-builder/layout'
-
 require 'rake'
 
 module PageBuilder
@@ -8,7 +7,7 @@ module PageBuilder
 
   # Defines and returns the "build" rake task.
   def self.rake_task(root_dir = ".")
-    @task ||= task :build do
+    @task ||= Rake::Task.define_task :build do
       Folder.new(root_dir).process
     end
   end
